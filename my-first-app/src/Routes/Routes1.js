4bdc992db9c9a23ddb82885134a5053714a40319
Sushma Gupta <sushma9munni@gmail.com>
2021-02-20 13:02:37 +0530

    updated

--- my-first-app/src/Routes/Routes1.js
+++ my-first-app/src/Routes/Routes1.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Route, Switch } from "react-router-dom"
-import Navbar from '../Components/Navbar'
+
 import Login from '../Components/Login'
 import Register from '../Components/Register'
-//import PrivateRoutes from "./PrivateRoutes"
-//import Home from './Home'
-//import FlatDetail from './FlatDetail'
+
 import Home from '../Components/Moviepage'
 import Post from '../Components/Post'
  import Edit from '../Components/Edit'
 function Routes() {
     return (
         <>
-            {/* <Route path="/" render={() => <Navbar />} /> */}
+           
             <Switch>
                 <Route path="/" exact render={() => <Register />} />
                 <Route path="/Login" render={(props) => <Login {...props} />} />
